ed46f231907e44e82f1c8dd171923d5e99f0f89b
mahloun <thms.no@gmail.com>
2017-07-16 23:19:09 +0200

    apple specifig, init file for macOS and clipboard for tmux

--- apple_init.sh
+++ apple_init.sh
@@ -1,2 +1,6 @@
 # disable the press-and-hold character palette & allow key repeat
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Expanded Save Panel
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true && \
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
